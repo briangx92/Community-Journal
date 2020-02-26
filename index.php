f54bf8a08c3452e742e5fa7c9a25009a9e98cbd8
Zach Lehman <zalehman182@stevenscollege.edu>
2020-02-26 08:23:34 -0500

    The html for the home page is complete -> The header, footer and login screen -> Logo also uploaded to the screen and for the favicon

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="Pictures/logo.png">
+    <title>Community Journal</title>
+</head>
+    <h1>Welcome to Community Journal</h1>  
+
+    <img src="Pictures/logo.png" alt="This is the logo of the company.">
+
+    <h2>Login</h2>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" class="btn" name = "login">Login</button>
+    <button type="submit" class="btn" name = "register">Register</button>
+
+  </form>
+</div>
+
+<footer>
+  <ul>
+    <li>Phone: 717-555-5555</li>
+    <br>
+    <li>Email: CommunityJournal@gmail.com</li>
+    <br>
+    <li>Fax: 171-123-4567</li>
+    <br>
+  </ul>
+</footer>
+
+</body>
+</html>
