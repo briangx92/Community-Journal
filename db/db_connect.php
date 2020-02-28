4864abd2cf811d699a9a29ea331e1b17831adf58
Tfink13 <tsfink182@stevenscollege.edu>
2020-02-28 09:11:43 -0500

    add .gitignore file

---
+++ db/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "community_journal";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+
+
+
+?>
