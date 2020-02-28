384ca311f04901a74b79f44de0a48b152b561e39
Zach Lehman <zalehman182@stevenscollege.edu>
2020-02-28 08:11:42 -0500

    Fixed the dashboard -> The Links are all connected to one another -> And Pictures are all working as plan

--- dashboard.php
+++ dashboard.php
@@ -5,19 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="style.css" rel="stylesheet" type="text/css">
-    <link rel="icon" href="photos/logo.png">
+    <link rel="icon" href="Pictures/logo.png">
 </head>
 <header>
-    <!-- Header Nav -->
     <nav>
         <ul>
-            <li><img src="/pictures/communityjournal.png" alt="logo" name="logo" id="logo"></li>
-            <li>Profile</li>
-            <li>Messages</li>
-            <li>Personal Page</li>
-            <li>Search</li>
-            <li>Profile</li>
-            <li><button type="submit" name="logout" alt="logout">Logout</button></li>
+            <li><a href="dashboard.php"><img src="Pictures/logo.png" alt="This is the logo of the company and it also doubles as a home button to the dashboard."></a></li>
+            <li><a href="messages.php">Messages</a></li>
+            <li><a href="personal.php">Personal</a></li>
+            <li><a href="public.php">Public</a></li>
+            <li><input type="text" name="search" placeholder="Search"></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li>
+                <form action="" class = "logout">
+                    <button type="submit" class="btn" name = "logout">Logout</button>
+                </form>
+            </li>
         </ul>
     </nav>
     <hr>
@@ -32,7 +35,7 @@
     </article>
     <section name="blog feed">
         <article name="blog1">
-            <img src="yourphotos/#" name="picture" alt="blog_pic">
+            <!-- <img src="yourphotos/#" name="picture" alt="blog_pic"> -->
             <p>Blog Title</p>
             <p>Blogger Name</p>
             <p>Date</p>
@@ -40,7 +43,7 @@
         </article>
         <hr>
         <article name="blog2">
-            <img src="yourphotos/#" name="picture" alt="blog_pic">
+            <!-- <img src="yourphotos/#" name="picture" alt="blog_pic"> -->
             <p>Blog Title</p>
             <p>Blogger Name</p>
             <p>Date</p>
