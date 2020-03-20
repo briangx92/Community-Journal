eb24c7a7599469ebae17f6765f2f7b05adef7c55
Tfink13 <tsfink182@stevenscollege.edu>
2020-03-20 14:11:06 -0400

    I have a message system working but its a little buggy because i think its changing users as you go to click on some ones name to chat in the list of users (it will eventually be a list of friends but for now i got all the users to display on the side)

--- include/login.php
+++ include/login.php
@@ -1,81 +1,46 @@
 <?php
-$email_err = $password_err = "";
+session_start();
+
+include("../db/db.php");
+
+	if(isset($_POST['sign_in'])){
+
+	$email = htmlentities(mysqli_real_escape_string($conn,$_POST['email']));
+	$pass = htmlentities(mysqli_real_escape_string($conn,$_POST['pass']));
+
+	$select_user = "SELECT * from users where email ='$email' AND password ='$pass'";
+
+	$query = mysqli_query($conn,$select_user);
+
+	$check_user = mysqli_num_rows($query);
+
+	if($check_user==1){
+
+  	$_SESSION['email']=$email;
+
+  	$update_msg = mysqli_query($conn, "UPDATE users SET log_in='Online' WHERE email='$email'");
+
+  	$user = $_SESSION['email'];
+  	$get_user = "SELECT * from users where email='$user'";
+  	$run_user = mysqli_query($conn,$get_user);
+  	$row=mysqli_fetch_array($run_user);
+
+  	$user_name = $row['username'];
+
+  	echo "<script>window.open('messages.php?user_name=$user_name','_self')</script>";
+
+	}
+	else {
+	echo "
+
+	<div class='alert alert-danger'>
+	  <strong>Check your email and password!</strong>
+	</div>
+
+	";
+	}
+
+	}
 
 
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Check if username is empty
-    if(empty(trim(@$_POST["email"]))){
-        $email_err = "Please enter username.";
-    } else{
-        $email = trim($_POST["email"]);
-    }
-
-    // Check if password is empty
-    if(empty(trim(@$_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-
-    }
-
-
-
-    // Validate credentials
-    if(empty($email_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT user_id, email, password FROM users WHERE email = ?";
-
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_email);
-
-            // Set parameters
-            $param_email = $email;
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $user_id, $email, $hash);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if($password == $hash){
-                            // Password is correct, so start a new session
-                            session_start();
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $user_id;
-                            // maybe?
-                            // $_SESSION['approved'] = $approved;
-                            header("location: http://localhost/Community-Journal/register.php");
-
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that email.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-
-    // Close connection
-    mysqli_close($conn);
-}
-
-
- ?>
+?>
